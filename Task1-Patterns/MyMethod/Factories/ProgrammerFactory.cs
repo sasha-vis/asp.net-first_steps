e120d5c5759c20e343b356c6a7f7e9aa34870d78
a.vysotski <a.vysotski.vironit@vironit.ru>
2021-12-17 16:43:44 +0300

    finish: mymethod

---
+++ Task1-Patterns/MyMethod/Factories/ProgrammerFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace MyMethod.Factories
+{
+    public static class ProgrammerFactory
+    {
+        public static string CreateEmployee(string type, string name)
+        {
+            return type + " " + name + " was created!";
+        }
+    }
+}
---
+++ Task1-Patterns/MyMethod/MyMethod.cs
@@ -0,0 +1,25 @@
+﻿using MyMethod.Factories;
+
+namespace MyMethod
+{
+    public static class MyMethodFactory
+    {
+        public static string CreatEmployees(string type, string name)
+        {
+            switch (type)
+            {
+                case "programmer":
+                    return ProgrammerFactory.CreateEmployee(type, name);
+                case "hr-manager":
+                    return ProgrammerFactory.CreateEmployee(type, name);
+                case "qa-engineer":
+                    return ProgrammerFactory.CreateEmployee(type, name);
+                case "data-scientist":
+                    return ProgrammerFactory.CreateEmployee(type, name);
+                case "business-analyst":
+                    return ProgrammerFactory.CreateEmployee(type, name);
+            }
+            return "\nError: The type of employee is incorrect!\n\nPlease, choose another type:\nprogrammer\nhr-manager\nqa-engineer\ndata-scientist\nbusiness analyst\n";
+        }
+    }
+}
